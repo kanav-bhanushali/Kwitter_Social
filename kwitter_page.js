843a7ffd5984152481a633328c2f3000403e6f09
kanav-bhanushali <71547881+kanav-bhanushali@users.noreply.github.com>
2022-06-18 15:53:58 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -28,7 +28,31 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+u_name=message_data['u_name'];
+message=message_data['message'];
+like=message_data['like'];
+name_with_tag="<h4>"+u_name+"<img src='tick.png' class='user_tick'></h4>";
+message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
+like_button="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='update_like(this.id)'>";
+span_with_tag="<span class='glyphicon glyphicon-thumbs-up'>like:"+like+"</span> </button> <hr>";
+row=name_with_tag+message_with_tag+like_button+span_with_tag;
+document.getElementById("output").innerHTML+=row;
 //End code
       } });  }); }
 getData();
+
+
+function update_like(message_id){
+      button_id=message_id;
+      likes=document.getElementById(button_id).value;
+      updated_likes=Number(likes)+1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like:updated_likes
+      });
+}
+
+function Logout(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html";
+      }
